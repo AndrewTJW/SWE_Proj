0b7d43a14c66d5c1e70fff0d9813661c198afde7
Affan Najiy <affannajiy@gmail.com>
2025-07-15 20:40:46 +0800

    Dummies
    Contact and About Us

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,10 +9,7 @@ const Footer = () => {
         <div className="w-4/5">
           <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            TikTok Shop is an integrated e-commerce feature within TikTok that allows users to discover, promote, and purchase products directly through short-form videos and live streams—blending entertainment with shopping for a seamless digital retail experience.
           </p>
         </div>
 
@@ -40,14 +37,14 @@ const Footer = () => {
           <div>
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
-              <p>+1-234-567-890</p>
+              <p>+60 12 345 6789</p>
               <p>teknologia05@gmail.com</p>
             </div>
           </div>
         </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm">
-        Copyright 2025 © Teknologia All Right Reserved.
+        Copyright 2025 © Teknologia All Rights Reserved.
       </p>
     </footer>
   );
