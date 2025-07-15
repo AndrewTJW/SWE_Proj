eb8f1f5420cdb3ab961eb5515bf6b2dbbf15dc6f
AndrewTJW <130200621+AndrewTJW@users.noreply.github.com>
2025-07-15 15:56:43 +0800

    finalize ? commit ?

--- TikTokShop/app/api/product/list/route.js
+++ TikTokShop/app/api/product/list/route.js
@@ -3,14 +3,16 @@ import connectDB from "@/config/db";
 import { NextResponse } from "next/server";
 //to get sellers product
 export async function GET(request) {
-    try {
+  try {
+    await connectDB();
 
-        await connectDB();
-
-        const products = await Product.find({})
-        return NextResponse.json({ success: true, message: "All items fetched successfully!", products })
-
-    } catch (err) {
-        return NextResponse.json({ success: false, message: err.message })
-    }
+    const products = await Product.find({});
+    return NextResponse.json({
+      success: true,
+      message: "All items fetched successfully!",
+      products,
+    });
+  } catch (err) {
+    return NextResponse.json({ success: false, message: err.message });
+  }
 }
