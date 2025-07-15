39dbe44a6fd5040cca73ac50d8bef4baea395b85
AndrewTJW <130200621+AndrewTJW@users.noreply.github.com>
2025-07-15 14:26:17 +0800

    fixed calculation error on batch processing

--- TikTokShop/app/api/order/create/route.js
+++ TikTokShop/app/api/order/create/route.js
@@ -16,27 +16,27 @@ export async function POST(request) {
     //calculate amount using items
     const amount = await items.reduce(async (acc, item) => {
       const product = await Product.findById(item.product);
-      return acc + product.offerPrice * item.quantity
+      return (await acc) + product.offerPrice * item.quantity;
     }, 0);
 
     await inngest.send({
-        name: 'order/created',
-        data: {
-            userId,
-            address,
-            items,
-            amount: amount + Math.floor(amount * 0.02),
-            date: Date.now()
-        }
-    })
+      name: "order/created",
+      data: {
+        userId,
+        address,
+        items,
+        amount: amount + Math.floor(amount * 0.02),
+        date: Date.now(),
+      },
+    });
 
-    const user = await User.findById(userId)
-    user.cartItems = {}
-    await user.save()
+    const user = await User.findById(userId);
+    user.cartItems = {};
+    await user.save();
 
-    return NextResponse.json({ success: true, message: 'Order Placed'})
+    return NextResponse.json({ success: true, message: "Order Placed" });
   } catch (err) {
-    console.log(error)
-    return NextResponse.json( {success: false, message: err.message} )
+    console.log(error);
+    return NextResponse.json({ success: false, message: err.message });
   }
 }
